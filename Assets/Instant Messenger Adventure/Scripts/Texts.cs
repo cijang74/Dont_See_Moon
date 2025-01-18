fd519991d892461d4809f7efe435db41746a0d34
Jaehoon Kim <cijang74@naver.com>
2025-01-18 23:35:59 +0900

    김재훈의 점심밥

---
+++ Assets/Instant Messenger Adventure/Scripts/Texts.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class Texts{
+
+	public string text; //texto da mensagem
+	public float waitTime = 1; //tempo que espera antes de digitar
+	public float typingTime = 2; //tempo para ela ser digitada
+
+}
