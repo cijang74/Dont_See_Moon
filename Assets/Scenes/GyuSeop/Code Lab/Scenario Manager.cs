7c90484000ff6aebb5270b8f45521386adab9531
CoziBit (songGyuSeob) <gs2412@naver.com>
2025-01-23 23:51:25 +0900

    시나리오 블록 진행 매니저 제작 기초닦기

---
+++ Assets/Scenes/GyuSeop/Code Lab/Scenario Manager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem.OSX;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScenarioManager : MonoBehaviour
+{
+    public int playedDay=0; //진행일차
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ProgressScenarioBlock()
+    {
+        
+
+        yield return null;
+    }
+}
+
